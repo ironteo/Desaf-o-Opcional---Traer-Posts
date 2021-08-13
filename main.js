b11982cc86faf39350f3a6f5ddd50f80c4598eb3
ironteo <33724250+ironteo@users.noreply.github.com>
2021-08-13 14:05:34 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+const urlBase = 'https://jsonplaceholder.typicode.com/posts';
+
+const request = async (url) => {
+     const results = await fetch(url);
+     const response = await results.json();
+     return response;
+}
+
+const getPost = async () => {
+    const url = `${urlBase}`;
+    return request(url);
+}
+
+getPost().then( resp => { 
+    console.log(resp);
+    const escribir = document.getElementById('post-data');
+    resp.forEach(element => {
+        const title = element.title;
+        const body = element.body;
+        escribir.innerHTML = `
+        <ul>
+            <li>${title} \n
+            ${body} </li>
+        </ul>
+    `;
+    console.log('title', title);
+    console.log('body', body)
+    });
+}).catch(err => console.log(err))
+
 
